931ebd9dad0237e1beab4baceeb1a17d95469987
shahidian <elnazshahidian@yahoo.com>
2020-05-27 19:24:54 +0430

    js file added, some links specified

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.pro').click(function(){
+        $('.ulp').toggle();
+    });
+});
+
+$(document).ready(function(){
+    $('.quo').click(function(){
+        $('.ulq').toggle(400);
+    });
+});
